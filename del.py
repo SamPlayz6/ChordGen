393e72fa086be20d7d9f2b4f7d5a1b8ab2d9dcb0
Sam-urai <44670760+SamPlayz6@users.noreply.github.com>
2024-05-08 10:13:26 +0100

    Added lakh pre-processsing code to  del.py

--- del.py
+++ del.py
@@ -23,3 +23,70 @@ plt.show()
 #Example Above of data processing + Extraction - Does not include pitch correction or removal of noise
 
 
+#------------------------
+
+import os
+import pretty_midi
+import numpy as np
+
+def extract_melody_and_chords(pm):
+    melody = []
+    chords = []
+    for instrument in pm.instruments:
+        if instrument.is_drum:
+            continue
+        for note in instrument.notes:
+            melody.append(note.pitch)
+            chords.append(pretty_midi.note_number_to_name(note.pitch))
+    return melody, chords
+
+
+def transpose_to_c_major(pm):
+    key = pm.key_signature_changes
+    if len(key) > 0:
+        key = key[0].key_number
+    else:
+        key = 0
+    semitones = -key
+    for instrument in pm.instruments:
+        for note in instrument.notes:
+            note.pitch += semitones
+    return pm
+
+def encode_melody_and_chords(melody, chords):
+    unique_notes = list(set(melody))
+    note_to_int = {note: i for i, note in enumerate(unique_notes)}
+    melody_encoded = [note_to_int[note] for note in melody]
+
+    unique_chords = list(set(chords))
+    chord_to_int = {chord: i for i, chord in enumerate(unique_chords)}
+    chords_encoded = [chord_to_int[chord] for chord in chords]
+
+    return melody_encoded, chords_encoded
+
+
+# Directory where the Lakh MIDI dataset is stored
+midi_dir = 'path_to_your_midi_files'
+
+# Iterate over all MIDI files in the directory
+for filename in os.listdir(midi_dir):
+    if filename.endswith('.mid'):
+        # Load the MIDI file
+        pm = pretty_midi.PrettyMIDI(os.path.join(midi_dir, filename))
+
+        # Extract melody and chords
+        melody, chords = extract_melody_and_chords(pm)
+
+        # Transpose to C major
+        pm = transpose_to_c_major(pm)
+
+    # Encode melody and chords
+        melody_encoded, chords_encoded = encode_melody_and_chords(melody, chords)
+
+        # Now melody_encoded and chords_encoded can be used as input and output for your LSTM model
+
+
+
+
+
+
