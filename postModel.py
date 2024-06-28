b411886f346c55d302707a4f5a9573349fd43255
SamPlayz6 <samddunning@hotmail.co.uk>
2024-06-28 11:49:04 +0100

    Now generating Melody and Melody + Chords MIDI for comparision

--- postModel.py
+++ postModel.py
@@ -1,105 +1,31 @@
-from music21 import stream, chord, midi, converter, note, meter
-#--------------------Combining MIDI-----------------------
+#-------------------Convert Melody to MIDI-----------Testing Purposes Only
+def create_melody_midi(melody_string, bpm):
+    mid = MidiFile()
+    track = MidiTrack()
+    mid.tracks.append(track)
 
-def combine_midi_files(melody_file, chords_file, output_file):
-    """ Combines melody and chord MIDI files into a single MIDI file. """
-    # Load the melody and chords MIDI files
-    melody_stream = converter.parse(melody_file)
-    chords_stream = converter.parse(chords_file)
+    # Set the tempo
+    track.append(MetaMessage('set_tempo', tempo=mido.bpm2tempo(bpm)))
 
-    # Ensure measures are added to the streams
-    melody_stream = add_measures(melody_stream)
-    chords_stream = add_measures(chords_stream)
+    ticks_per_beat = mid.ticks_per_beat
+    melody_notes = melody_string.split(',')
 
-    # Create a new stream for the combined output
-    combined_stream = stream.Score()
-    combined_stream.append(melody_stream)
-    combined_stream.append(chords_stream)
+    for i, note_str in enumerate(melody_notes):
+        note_number = note_to_midi(note_str)
+        velocity = 64  # standard velocity
+        time = 0
+        
+        # Note on
+        track.append(Message('note_on', note=note_number, velocity=velocity, time=time))
+        # Note off, here we assume each note lasts for one beat
+        track.append(Message('note_off', note=note_number, velocity=velocity, time=ticks_per_beat))
 
-    # Write the combined stream to a new MIDI file
-    midi_file = midi.translate.streamToMidiFile(combined_stream)
-    midi_file.open(output_file, 'wb')
-    midi_file.write()
-    midi_file.close()
-    print(f"Combined MIDI file '{output_file}' has been saved.")
-
-
-def add_measures(midi_stream):
-    """ Encloses notes in measures for proper MIDI structuring. """
-    measures = stream.Part()
-    measures.append(meter.TimeSignature('4/4'))
-    for element in midi_stream.flat.notesAndRests:
-        measures.append(element)
-    measures.makeMeasures(inPlace=True)
-    return measures
-
-
-#------------------ String to MIDI---------------------
-
-
-from music21 import note, chord
-
-def chord_notes(root, quality):
-    """ Generate the correct notes for a given chord root and quality with default velocity. """
-    notes = []
-    velocity = 64  # A sensible default velocity for audible notes
-    # Define each chord type with appropriate intervals and ensure the velocity is set
-    if quality == 'major':
-        notes = [note.Note(root+'3', volume=velocity), 
-                 note.Note(root+'5', volume=velocity), 
-                 note.Note(root+'8', volume=velocity)]
-    elif quality == 'minor':
-        notes = [note.Note(root+'b3', volume=velocity), 
-                 note.Note(root+'5', volume=velocity), 
-                 note.Note(root+'8', volume=velocity)]
-    elif quality == 'dominant':
-        notes = [note.Note(root+'3', volume=velocity), 
-                 note.Note(root+'5', volume=velocity), 
-                 note.Note(root+'b7', volume=velocity)]
-    elif quality == 'major-seventh':
-        notes = [note.Note(root+'3', volume=velocity), 
-                 note.Note(root+'5', volume=velocity), 
-                 note.Note(root+'7', volume=velocity)]
-    elif quality == 'minor-seventh':
-        notes = [note.Note(root+'b3', volume=velocity), 
-                 note.Note(root+'5', volume=velocity), 
-                 note.Note(root+'b7', volume=velocity)]
-    return chord.Chord(notes)
+    # Save the MIDI file
+    mid.save('TestMelodies/Outputs/melody_midi.mid')
 
 
 
-
-from music21 import stream, midi, converter, meter
-
-def change_chords_to_midi(chord_string, timings, melody_file, output_file):
-    melody_stream = converter.parse(melody_file)
-    total_duration = melody_stream.duration.quarterLength  # Get total length of the melody in quarter lengths
-    
-    chords_part = stream.Part()
-    time_signature = melody_stream.recurse().getElementsByClass(meter.TimeSignature)[0]
-    chords_part.append(time_signature)  # Copy time signature from melody
-
-    chord_entries = chord_string.split(',')
-    for idx, chord_entry in enumerate(chord_entries):
-        root, quality = chord_entry.split('/')
-        new_chord = chord_notes(root, quality)
-        new_chord.duration.quarterLength = 4  # Adjust duration as needed
-        scaled_timing = (timings[idx] / max(timings)) * total_duration
-        new_chord.offset = scaled_timing  # Set offset based on scaled timing
-        chords_part.append(new_chord)
-
-    combined_stream = stream.Score()
-    combined_stream.append(melody_stream.parts[0])  # Assuming melody is single-part
-    combined_stream.append(chords_part)
-
-    midi_file = midi.translate.streamToMidiFile(combined_stream)
-    midi_file.open(output_file, 'wb')
-    midi_file.write()
-    midi_file.close()
-    print(f"Combined MIDI file '{output_file}' has been saved.")
-
-
-#-------------------Melody + Chord String + BMP to MIDI----------Retrying above functions
+#-------------------Melody + Chord String + BMP to MIDI-------------------
 import mido
 from mido import MidiFile, MidiTrack, Message, MetaMessage
 
@@ -151,57 +77,63 @@ def create_midi(melody_string, chord_string, bpm):
     mid = MidiFile()
     track = MidiTrack()
     mid.tracks.append(track)
-    track.append(MetaMessage('set_tempo', tempo=mido.bpm2tempo(bpm)))
 
     melody_notes = melody_string.split(',')
     chord_progressions = chord_string.split(',')
+
     ticks_per_beat = mid.ticks_per_beat
+    # ticks_per_step = int(ticks_per_beat * (60 / bpm))
+
+    track.append(MetaMessage('set_tempo', tempo=mido.bpm2tempo(bpm)))
+
+    current_chord_notes = None
     time_since_last_note = 0
 
-    current_chord = []
     for melody_note, chord in zip(melody_notes, chord_progressions):
-        new_chord = chord_to_notes(chord)
-        if new_chord != current_chord:
-            # Turn off the previous chord
-            for note in current_chord:
-                track.append(Message('note_off', note=note, velocity=64, time=0))
-            # Turn on the new chord
-            for note in new_chord:
-                track.append(Message('note_on', note=note, velocity=64, time=time_since_last_note))
-            current_chord = new_chord
-            time_since_last_note = 0  # Reset time offset for chord changes
+        new_chord_notes = chord_to_notes(chord)
+        melody_note_midi = note_to_midi(melody_note)
 
-        # Process the melody note
-        if melody_note != "PAD":
-            melody_note_midi = note_to_midi(melody_note)
-            if melody_note_midi is not None:
-                track.append(Message('note_on', note=melody_note_midi, velocity=64, time=time_since_last_note))
-                track.append(Message('note_off', note=melody_note_midi, velocity=64, time=ticks_per_beat))
-                time_since_last_note = 0  # Reset time offset for melody notes
+        # Process Chord Changes
+        if new_chord_notes != current_chord_notes:
+            if current_chord_notes is not None:
+                for note in current_chord_notes:
+                    track.append(Message('note_off', note=note, velocity=64, time=0))
+            for note in new_chord_notes:
+                track.append(Message('note_on', note=note, velocity=64, time=time_since_last_note))
+            current_chord_notes = new_chord_notes
+            time_since_last_note = 0
+
+        # Process Melody Note
+        if melody_note_midi is not None:
+            track.append(Message('note_on', note=melody_note_midi, velocity=64, time=time_since_last_note))
+            track.append(Message('note_off', note=melody_note_midi, velocity=64, time=ticks_per_beat))
+            time_since_last_note = 0
 
     # Ensure all notes are turned off at the end
-    for note in current_chord:
-        track.append(Message('note_off', note=note, velocity=64, time=0))
+    if current_chord_notes:
+        for note in current_chord_notes:
+            track.append(Message('note_off', note=note, velocity=64, time=0))
 
-    mid.save('SungMelodies/Outputs/output_midi.mid')
+    mid.save('TestMelodies/Outputs/song_midi.mid')
 
 
 # Example usage
 if __name__ == "__main__":
     melody_string = "C0/6,C1/6,C0/6,B0/5,A0/5,G0/5,F1/5,D0/5,F1/5,E0/5,B0/5,F0/5,F1/5,G0/5,A0/5,F1/5,G0/5,A0/5,F1/5,G0/5,A0/5,G0/5,D0/6,B0/5,A0/5,G0/5,D0/5,E0/5,G0/5,F1/5,C1/6,C1/6,C0/6,B0/5,C1/5,D0/5,E0/5,D0/5,D0/5,F1/5,A0/5,C1/6,E0/6,D0/6,D0/5,C1/5,D0/5,E0/6,E0/6,D0/6,B0/5,A0/5,F1/5"
     chord_string = "PAD,E0/dominant,E0/dominant,E0/minor-seventh,E0/minor-seventh,B0/minor-seventh,E0/minor-seventh,B0/minor-seventh,B0/minor-seventh,B0/minor-seventh,E0/dominant,E0/dominant,B0/minor-seventh,A0/minor-seventh,A0/minor-seventh,D0/dominant,A1/diminished,A0/minor-seventh,A0/minor-seventh,A0/minor-seventh,D0/dominant,D0/dominant,G0/major-seventh,G0/major-seventh,G0/major-seventh,E0/dominant,A0/minor-seventh,A0/minor-seventh,D0/dominant,A1/diminished,A0/minor-seventh,A0/minor-seventh,D0/dominant,A1/diminished,A1/diminished,A1/diminished,A0/minor-seventh,A0/minor-seventh,A0/minor-seventh,A0/minor-seventh,A0/minor-seventh,A0/minor-seventh,A1/diminished,A0/minor-seventh,A0/minor-seventh,D0/dominant,A1/diminished,A1/diminished,A1/diminished,A0/minor-seventh,A0/minor-seventh,A0/minor-seventh,A1/diminished"
-    bpm = 120
+    bpm = 110
+
+
     create_midi(melody_string, chord_string, bpm)
-
-
+    create_melody_midi(melody_string,bpm)
 
     # chord_string = "E0/dominant,E0/dominant,E0/minor-seventh,E0/minor-seventh,B0/minor-seventh,E0/minor-seventh,B0/minor-seventh,B0/minor-seventh,B0/minor-seventh,E0/dominant,E0/dominant,B0/minor-seventh,A0/minor-seventh,A0/minor-seventh,D0/dominant,A1/diminished,A0/minor-seventh,A0/minor-seventh,A0/minor-seventh,D0/dominant,D0/dominant,G0/major-seventh,G0/major-seventh,G0/major-seventh,E0/dominant,A0/minor-seventh,A0/minor-seventh,D0/dominant,A1/diminished,A0/minor-seventh,A0/minor-seventh,D0/dominant,A1/diminished,A1/diminished,A1/diminished,A0/minor-seventh,A0/minor-seventh,A0/minor-seventh,A0/minor-seventh,A0/minor-seventh,A0/minor-seventh,A1/diminished,A0/minor-seventh,A0/minor-seventh,D0/dominant,A1/diminished,A1/diminished,A1/diminished,A0/minor-seventh,A0/minor-seventh,A0/minor-seventh,A1/diminished"
-    # melody_file_path = "SungMelodies/Inputs/melody.mid"
-    # output_file_path = "SungMelodies/Outputs/chords.mid"
+    # melody_file_path = "TestMelodies/Inputs/melody.mid"
+    # output_file_path = "TestMelodies/Outputs/chords.mid"
     # change_chords_to_midi(chord_string, timings, melody_file_path, output_file_path)
 
 
     # # Combining the Melody and Chord MIDIs
-    # chords_file = "SungMelodies/Outputs/chords.mid"
-    # output_file = "SungMelodies/Outputs/combined_output.mid"
+    # chords_file = "TestMelodies/Outputs/chords.mid"
+    # output_file = "TestMelodies/Outputs/combined_output.mid"
     # combine_midi_files(melody_file, chords_file, output_file)
