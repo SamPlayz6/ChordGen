3489f0dae32e4c3fa882064e1f5eb9894c0e84c9
SamPlayz6 <44670760+SamPlayz6@users.noreply.github.com>
2024-07-04 16:45:10 +0100

    Need to make the parameters more accesable for preprocessing and then try to alter them to work better

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,44 +1,3 @@
-from music21 import converter
-from mido import MidiFile
-
-#--------------------XML to MIDI(Not Part Of Flow)-------------------
-
-def convert_xml_to_midi(xml_file_path, midi_file_path):
-    # Load the MusicXML file
-    score = converter.parse(xml_file_path)
-    
-    # Save as MIDI
-    midi = score.write('midi', fp=midi_file_path)
-    print(f"MIDI file saved as {midi_file_path}")
-
-
-
-#--------------MIDI to String(Of notes)(Not Part Of Flow)-------------
-
-def midi_to_note_string(midi_file, ticks_per_beat=480):
-    midi = MidiFile(midi_file)
-    note_events = []
-    current_time = 0
-    note_string = []
-
-    for track in midi.tracks:
-        for msg in track:
-            if msg.type == 'note_on' and msg.velocity > 0:
-                note_events.append((msg.note, current_time))
-            if not msg.is_meta:
-                current_time += msg.time
-
-    # Convert note numbers to note names
-    note_names = ['C0', 'C1', 'D0', 'D1', 'E0', 'F0', 'F1', 'G0', 'G1', 'A0', 'A1', 'B0']
-    for note, time in note_events:
-        note_name = note_names[note % 12]
-        octave = note // 12 - 1  # MIDI note 60 is middle C (C4), which corresponds to note_name[0] in octave 4
-        note_string.append(f"{note_name}/{octave}")
-
-    return ",".join(note_string)
-
-
-#-----------------AudioToMIDI(Or StringOfNotes)------------
 import librosa
 import numpy as np
 from midiutil import MIDIFile
@@ -100,16 +59,3 @@ def wav_to_midi_improved(input_wav, output_midi, min_duration=0.1):
     # Save the MIDI file
     with open(output_midi, "wb") as output_file:
         midi.writeFile(output_file)
-
-
-if __name__ == "__main__":
-    # Use the function with the specified input and output paths
-    wav_to_midi_improved('TestMelodies/M1.wav', 'TestMelodies/Inputs/ModelInputMIDI.mid')
-
-    # Example usage
-    #convert_xml_to_midi('C://Users//user//Documents//GitHub//ChordGen//Example//as.xml', 'C://Users//user//Documents//GitHub//ChordGen//Example//test.mid')
-
-    # # Converting Melody MIDI to Melody String Sequence
-    # midi_file = "TestMelodies/Inputs/melody.mid"
-    # note_string = midi_to_note_string(midi_file)
-    # print(note_string)
